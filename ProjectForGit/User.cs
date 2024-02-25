7b94a19546eddedf695fa721f8fe60dd49b50f8c
Admin <Admin@mymail>
2024-02-25 22:58:13 +0500

    Added constructor for User class

--- ProjectForGit/User.cs
+++ ProjectForGit/User.cs
@@ -11,5 +11,7 @@ namespace ProjectForGit
         public required string UserName { get; set; }
         public required string Password { get; set; }
         public required string Email { get; set; }
+        public User() { }
+
     }
 }
