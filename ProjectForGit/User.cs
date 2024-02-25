2ab411361f3c9c492310ed245e5945c238d2bbe2
Admin <Admin@mymail>
2024-02-25 22:35:27 +0500

    Added User class

---
+++ ProjectForGit/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectForGit
+{
+    internal class User
+    {
+        public required string UserName { get; set; }
+        public required string Password { get; set; }
+        public required string Email { get; set; }
+    }
+}
