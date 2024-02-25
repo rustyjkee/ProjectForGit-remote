8898fd77079b2c257dbecaa3631b7c4813e65fd7
Admin <Admin@mymail>
2024-02-25 23:00:06 +0500

    Revert "Added constructor for User class"
    This reverts commit 7b94a19546eddedf695fa721f8fe60dd49b50f8c.

--- ProjectForGit/User.cs
+++ ProjectForGit/User.cs
@@ -11,7 +11,5 @@ namespace ProjectForGit
         public required string UserName { get; set; }
         public required string Password { get; set; }
         public required string Email { get; set; }
-        public User() { }
-
     }
 }
